4d8d9380dfda00c7a8d814131e017b11bf0dc39b
bakhitae <endribak@gmail.com>
2024-09-17 00:31:34 +0700

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -11,7 +11,7 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
 )
 # -------------------------------------- Read dataset ------------------------------
-df = pd.read_pickle("data_car_prices.pkl")
+df = pd.read_pickle("data_car_prices.pkl.gz")
 # -------------------------------------- Membuat Sidebar --------------------
 with st.sidebar:
     # Menambahkan Logo Pribadi
